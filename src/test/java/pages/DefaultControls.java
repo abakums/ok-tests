b243eaafb86a017b2da42aace913f119876e1291
lavrentiev-d <denlavre@mail.ru>
2024-01-17 12:39:29 +0300

    Добавен DefaultControls для кнопок действия модуля

---
+++ src/test/java/pages/DefaultControls.java
@@ -0,0 +1,13 @@
+package pages;
+
+import static com.codeborne.selenide.Selenide.$;
+
+import com.codeborne.selenide.SelenideElement;
+
+import io.appium.java_client.MobileBy;
+
+public class DefaultControls {
+
+    private SelenideElement notification = $(MobileBy.xpath("//android.view.ViewGroup[@content-desc=\"Notifications\"]"));
+    private SelenideElement search = $(MobileBy.xpath("//android.view.ViewGroup[@content-desc=\"Menu\"]"));
+}
